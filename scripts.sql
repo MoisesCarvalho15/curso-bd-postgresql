822e64c8572e521606a125f149b381c417dbcfbf
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2025-02-26 12:03:50 -0300

    Update - Métodos de consultas na tabela

--- scripts.sql
+++ scripts.sql
@@ -21,7 +21,7 @@ create table cliente (
 	constraint pk_cln_idcliente primary key (idcliente)
 );
 
--- Inserção de dados
+-- ### Inserção de dados ### 
 insert into cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento,bairro, municipio, uf)
 values (1, 'Manoel', '88828383821', '32323', '2001/01/30', 'M', 'Estudante', 'Brasileira', 'Rua Joaquim Nabuco', '23', 'Casa', 'Cidade Nova', 'Porto Uniao', 'SC');
 
@@ -66,3 +66,41 @@ values (14, 'Jeferson', null, null, '1983/07/01', 'M', null, 'Brasileiro', null,
 
 insert into cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento,bairro, municipio, uf)
 values (15, 'Jessica', null, null, null, 'F', 'Estudante', null, null, null, null, null, 'União Vitória', 'PR');
+
+-- ### Consultas na Tabela ###
+
+-- Consultando a tabela inteira
+select * from cliente;
+
+-- Consultando a tabela inteira com um limite
+select * from cliente limit 3;
+
+-- Consultando campos específicos
+select nome, data_nascimento from cliente;
+
+-- Renomeando a exibição de uma coluna
+select nome, data_nascimento as "Data de Nascimento" from cliente;
+
+-- Concatenando colunas e renomeando
+select 'CPF: ' || cpf || ' RG: ' || rg  as "CPF e RG" from cliente;
+
+-- Filtrando por condições
+select nome, data_nascimento from cliente where data_nascimento > '2000-01-01';
+
+-- Filtrando por nomes que começam com uma letra específica
+select nome from cliente where nome like 'C%';
+
+-- Filtrando por nomes que contenham uma letra específica no nome
+select nome from cliente where nome like '%c%';
+
+-- Filtrando por dados onde esteja entre duas condições
+select nome, data_nascimento from cliente where data_nascimento between '1990-01-01' and '1998-01-01';
+
+-- Filtrando somente clientes que estejam com o compo de rg vazio
+select nome, rg from cliente where rg is null;
+
+-- Ordenando os nomes em ordem alfabética
+select nome from cliente order by nome asc;
+
+-- Ordenando os nomes em descendente
+select nome from cliente order by nome desc;
