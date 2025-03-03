738fa8ab0fd9cd193ccc3820e4580dcb88cc619a
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2025-03-03 11:14:32 -0300

    Criando e inserindo dados na tabela nacionalidade

---
+++ scripts/tabela_nacionalidade.sql
@@ -0,0 +1,19 @@
+create table nacionalidade (
+	idnacionalidade integer not null,
+	nome varchar(30) not null,
+
+	constraint pk_ncn_idnacionalidade primary key (idnacionalidade),
+	constraint un_ncn_nome unique (nome)
+);
+
+-- Visualizando as nacionalidades da tabela de clientes
+select nacionalidade from cliente;
+
+-- Inserindo as nacionalidades na tabela de nacionalidade
+insert into nacionalidade (idnacionalidade, nome) values (1, 'Brasileira');
+insert into nacionalidade (idnacionalidade, nome) values (2, 'Italiana');
+insert into nacionalidade (idnacionalidade, nome) values (3, 'Norte-americana');
+insert into nacionalidade (idnacionalidade, nome) values (4, 'Alemã');
+
+-- Visualizando a tabela nacionalidade
+select * from nacionalidade;
