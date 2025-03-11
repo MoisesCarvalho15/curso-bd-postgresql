bf7266a163a94c9de99dd3afd4fc633d80c3c5d7
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2025-03-11 10:13:06 -0300

    feat: Exercícios de Funções Agregadas

--- scripts/exercicios_funcoes_agregadas.sql
+++ scripts/exercicios_funcoes_agregadas.sql
@@ -60,3 +60,67 @@ select idvendedor, sum(valor) from pedido group by idvendedor;
 
 -- 20. O somatório do valor do pedido agrupado por transportadora.
 select idtransportadora, sum(valor) from pedido group by idtransportadora;
+
+-- 21. O somatório do valor do pedido agrupado pela data. 
+select data_pedido, sum(valor) from pedido group by data_pedido;
+
+-- 22. O somatório do valor do pedido agrupado por cliente, vendedor e transportadora.
+select idcliente, idvendedor, idtransportadora, sum(valor)  from pedido group by idcliente, idvendedor, idtransportadora;
+
+-- 23. O somatório do valor do pedido que esteja entre 01/04/2008 e 10/12/2009 e que seja maior que R$ 200,00.
+select sum(valor) from pedido where data_pedido between '2008-04-01' and '2009-12-10'and valor > 200;
+
+-- 24. A média do valor do pedido do vendedor André.
+select * from vendedor; -- id André = 1
+select avg(valor) from pedido where idvendedor = 1;
+
+-- 25. A média do valor do pedido da cliente Jéssica.
+select * from cliente; -- id jéssica = 15
+select avg(valor) from pedido where idcliente = 15;
+
+-- 26. A quantidade de pedidos transportados pela transportadora BS. Transportes.
+select * from transportadora; -- id bs = 1
+select count(idcliente) from pedido where idtransportadora = 1;
+
+-- 27. A quantidade de pedidos agrupados por vendedor.
+select idvendedor, count(idpedido) from pedido group by idvendedor;
+
+-- 28. A quantidade de pedidos agrupados por cliente.
+select idcliente, count(idpedido) from pedido group by idcliente;
+
+-- 29. A quantidade de pedidos entre 15/04/2008 e 25/04/2008.
+select count(data_pedido) from pedido where data_pedido between '2008-04-15' and '20080-04-25';
+
+-- 30. A quantidade de pedidos que o valor seja maior que R$ 1.000,00.
+select count(valor) from pedido where valor > 1000;
+
+-- 31. A quantidade de microcomputadores vendida.
+select * from produto; -- id 1
+select sum(quantidade) from pedido_produto where idproduto = 1;
+
+-- 32. A quantidade de produtos vendida agrupado por produto.
+select idproduto, count(quantidade) from pedido_produto group by idproduto;
+
+-- 33. O somatório do valor dos produtos dos pedidos, agrupado por pedido.
+select idpedido, sum(valor_unitario) from pedido_produto group by idpedido;
+
+-- 34. A quantidade de produtos agrupados por pedido.
+select idpedido, sum(quantidade) from pedido_produto group by idpedido;
+
+-- 35. O somatório dos valores unitários de todos os produtos.
+select sum(valor_unitario) from pedido_produto;
+
+-- 36. A média dos produtos do pedido 6.
+select avg(valor_unitario) from pedido_produto where idpedido = 6;
+
+-- 37. O valor do maior produto do pedido.
+select max(valor_unitario) from pedido_produto;
+
+-- 38. O valor do menor produto do pedido.
+select min(valor_unitario) from pedido_produto;
+
+-- 39. O somatório da quantidade de produtos por pedido.
+select idpedido, sum(quantidade) from pedido_produto group by idpedido;
+
+-- 40. O somatório da quantidade de todos os produtos do pedido.
+select sum(quantidade) from pedido_produto;
